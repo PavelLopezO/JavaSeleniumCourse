f6b7ed86a617ed76e4e2bb3331f5221bc505909d
Pavel Lopez <plopez@advancio.com>
2023-01-25 20:02:53 -0800

    HomeWork 3.1

--- src/test/java/firstCodeClass/test1.java
+++ src/test/java/firstCodeClass/test1.java
@@ -13,18 +13,41 @@ public class test1 {
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
 
+        //Declare elements from Login Page
         WebElement usernameInput = driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
         WebElement passwordInput = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
 
+        //Send Username and Password, click on login button
         usernameInput.sendKeys("standard_user");
         passwordInput.sendKeys("secret_sauce");
         loginButton.click();
 
+        //Declare SauceLabsBackpack_label Label for assertion
         WebElement SauceLabsBackpack_label = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div"));
 
         //Use the code below, to match the expected text of the first item in the shop
         Assert.assertEquals("Sauce Labs Backpack",SauceLabsBackpack_label.getText());
+
+        //Declare items to add to the cart (SauceLabsBackpack_AddtoCartButton and SauceLabsBikeLight_AddtoCartButton) and CartButton
+        WebElement SauceLabsBackpack_AddtoCartButton = driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]"));
+        WebElement SauceLabsBikeLight_AddtoCartButton = driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-bike-light\"]"));
+        WebElement CartButton = driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a"));
+
+        //Click on both elements declared and the go to the Cart page
+        SauceLabsBackpack_AddtoCartButton.click();
+        SauceLabsBikeLight_AddtoCartButton.click();
+        CartButton.click();
+
+        WebElement SauceLabsBackpack_Incart = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div"));
+        WebElement SauceLabsBackpackSauceLabsBikeLight_Incart = driver.findElement(By.xpath("//*[@id=\"item_0_title_link\"]/div"));
+
+
+        Assert.assertEquals("Sauce Labs Backpack",SauceLabsBackpack_Incart.getText());
+        Assert.assertEquals("Sauce Labs Bike Light",SauceLabsBackpackSauceLabsBikeLight_Incart  .getText());
+
+
+
         System.out.println("Everything works");
 
 
