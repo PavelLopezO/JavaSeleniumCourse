630af3082a3812e3951fa16a6d4d3c761c4a79d2
Pavel Lopez <plopez@advancio.com>
2023-01-27 19:55:30 -0800

    Homework 3.1 and 3.2

--- src/test/java/firstCodeClass/test1.java
+++ src/test/java/firstCodeClass/test1.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class test1 {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe"  );
@@ -34,20 +36,30 @@ public class test1 {
         WebElement SauceLabsBikeLight_AddtoCartButton = driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-bike-light\"]"));
         WebElement CartButton = driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a"));
 
+        //Declare elements by Class and print the text of all the labels one by one
+        List<WebElement> allLabels = driver.findElements(By.className("inventory_item_name"));
+        for(int i = 0; i < allLabels.size(); i++)
+        {
+            System.out.println("Element " + i + " " + allLabels.get(i).getText());
+        }
+        System.out.println("Homework 3.1, Done");
+
+
         //Click on both elements declared and the go to the Cart page
+
         SauceLabsBackpack_AddtoCartButton.click();
         SauceLabsBikeLight_AddtoCartButton.click();
         CartButton.click();
 
+        //Declare labels on products from inside the Cart
         WebElement SauceLabsBackpack_Incart = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div"));
         WebElement SauceLabsBackpackSauceLabsBikeLight_Incart = driver.findElement(By.xpath("//*[@id=\"item_0_title_link\"]/div"));
 
-
+        //validate if names of Labels are in the Cart
         Assert.assertEquals("Sauce Labs Backpack",SauceLabsBackpack_Incart.getText());
         Assert.assertEquals("Sauce Labs Bike Light",SauceLabsBackpackSauceLabsBikeLight_Incart  .getText());
 
-
-
+        System.out.println("Homework 3.2, Done");
         System.out.println("Everything works");
 
 
